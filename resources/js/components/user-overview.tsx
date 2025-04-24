bac8bc51bbec140aa655f334e78aa9639ec6869b
matagaralph <matagaralph@gmail.com>
2025-04-24 10:59:53 +0200

    add document upload form

--- resources/js/components/user-overview.tsx
+++ resources/js/components/user-overview.tsx
@@ -1,8 +1,16 @@
-import { Link } from "@inertiajs/react";
+import { Link, useForm } from "@inertiajs/react";
 import { ColumnDef } from "@tanstack/react-table";
 import { Badge } from '@/components/ui/badge';
 import { DataTable } from "./DataTable/data-table";
 import { statuses, formatShortDate } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+import { FormEvent } from "react";
+import { FilePlus2 } from "lucide-react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+
 
 export default function UserOverView({ documents }: { documents: Document[] }) {
 
@@ -55,7 +63,88 @@ export default function UserOverView({ documents }: { documents: Document[] }) {
 
     return (
         <div className="absolute inset-0 size-full">
+            <div className="flex my-4 items-center justify-between">
+                <h2 className="text-xl font-semibold">My Documents</h2>
+                <UploadDocumentFrm />
+            </div>
             <DataTable data={documents} columns={columns} />
         </div>
+    );
+}
+
+function UploadDocumentFrm() {
+    const { data, setData, post, processing } = useForm({
+        title: '',
+        description: '',
+        file: null as File | null,
+    });
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        post('/documents/upload', {
+            forceFormData: true, // Important for file uploads
+        });
+    };
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button size='sm'>
+                    <FilePlus2 />
+                    Upload
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Request Submission</DialogTitle>
+                </DialogHeader>
+                <form onSubmit={handleSubmit}>
+                    <div className="grid gap-4 py-4">
+                        <div>
+                            <Label htmlFor="name" className="text-right mb-3">
+                                Title
+                            </Label>
+                            <Input
+                                id="name"
+                                name="name"
+                                className="mt-1.5"
+                                value={data.title}
+                                onChange={(e) => setData('title', e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <Label htmlFor="email" className="text-right">
+                                File
+                            </Label>
+                            <Input
+                                className="mt-1.5"
+                                id="email"
+                                name="email"
+                                type="file"
+                                onChange={(e) => setData('file', e.target.files?.[0] ?? null)}
+                                accept="application/pdf"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <Label htmlFor="email">
+                                Message
+                            </Label>
+                            <Textarea className="mt-2" rows={5} value={data.description} onChange={(e) => setData('description', e.target.value)} />
+                        </div>
+
+                    </div>
+                    <DialogFooter>
+                        <DialogClose>
+                            <Button size='sm' disabled={processing} type="submit">
+                                {processing ? 'Uploading...' : 'Upload Document'}
+                            </Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
     );
 }
