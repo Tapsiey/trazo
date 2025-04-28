000e305f8f4fe9c55fbdf2bdef9d723572083ad6
matagaralph <matagaralph@gmail.com>
2025-04-28 10:46:14 +0200

    add storage link under build commands

--- build-app.sh
+++ build-app.sh
@@ -15,3 +15,6 @@ php artisan config:cache
 php artisan event:cache
 php artisan route:cache
 php artisan view:cache
+php artisan storage:link
+php artisan db:seed --class=RolesAndPermissionsSeeder
+
